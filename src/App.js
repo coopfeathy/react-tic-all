8ad6224252126710ecd5f0c21193a2bfb521c293
coopfeathy <44649389+coopfeathy@users.noreply.github.com>
2020-03-06 12:04:41 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+import "./index.css"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            This is a template for a React app
+          </p>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
